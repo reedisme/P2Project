25345931e4c0b45ee7ea3643f2c313012c7c035f
reedisme <reedyoder1@gmail.com>
2017-11-27 10:57:44 +0000

    fixed friends list

--- leafmedia.py
+++ leafmedia.py
@@ -1,28 +1,4 @@
 import pickle
-
-class Graph:
-    def __init__(self):
-        self.matrix = {}
-
-    def add_user(self, key):
-        self.matrix[key] = []
-
-    def add_friendship(self, v1, v2):
-        if v1 in self.matrix and v2 in self.matrix:
-            self.matrix[v1].append(v2)
-            self.matrix[v1].sort()
-        else:
-            print("ERROR: One or both vertices do not yet exist")
-        print(self.matrix)
-
-    def is_edge(self, v1, v2):
-        if v2 in self.matrix[v1]:
-            return True
-        else:
-            return False
-
-friendsdb = Graph()
-
 #Python mock social media website. We start by creating a user class
 class User:
     """This Class defines a user in our domain."""
@@ -35,32 +11,19 @@ class User:
         # self.last      = last
         # self.birthday  = birthday
         self.friends   = []
-        friendsdb.add_user(self.userid)
 
     def add_friend(self, friend):
-        if self.userid > friend.userid:
-            friendsdb.add_friendship(self.userid, friend.userid)
-        elif friend.userid > self.userid:
-            friendsdb.add_friendship(friend.userid, self.userid)
+        self.friends.append(friend)
 
-    def are_friends(self, friend):
-        if self.userid > friend.userid:
-            return friendsdb.is_edge(self.userid, friend.userid)
-        elif friend.userid > self.userid:
-            return friendsdb.is_edge(friend.userid, self.userid)
+    def show_username(self):
+        return self.username
 
-    def generate_flist(self):
-        flist = []
-        for i in friendsdb.matrix[self]:
-            flist.append(i)
-        for i in friendsdb.matrix:
-            if self.userid in friendsdb.matrix[i]:
-                flist.append(i)
-        return(flist)
+    def show_friends(self):
+        for i in self.friends:
+            print(i.show_username())
 
 #Storing our userbase and users to the pickle file 'data.pk.'
 PIK = 'data.pk'
-
 
 with open(PIK, "rb") as f:
     objects = pickle.load(f)
@@ -69,6 +32,7 @@ userid, userbase = objects[0], objects[1]
 #userid, userbase = 0, []
 
 start = int(input("Welcome to LeaFacebook.\n Press 1 to sign up.\n Press 2 to login "))
+
 def getId(n):
     for i in userbase:
         if i[1] == n:
@@ -84,37 +48,46 @@ def lookup(id):
     print("This is not a valid username")
 
 def login():
-    username = raw_input("Please input your username: ")
-    password = raw_input("Please input your password: ")
+    username = input("Please input your username: ")
+    password = input("Please input your password: ")
     id = getId(username)
     passwd = lookup(id)
     if passwd == password:
-        print("hooray")
+        for i in userbase:
+            if username == i[1]:
+                return i[0]
     else:
         print("Please try again")
         login()
 
 def userGenerator():
     global userid
-    username = raw_input("Please input a username: ")
-    password = raw_input("Please input a password: ")
+    username = input("Please input a username: ")
+    password = input("Please input a password: ")
     userid += 1
     user = User(username, password, userid)
     userbase.append((user, username, password, userid))
     return userbase
+
+def home():
+    for i in range(100):
+        print(" ")
 
 def main(s):
     if s == 1:
         userGenerator()
     elif s == 2:
         login()
-print(userbase)
+        home()
+
 main(start)
 
 saveObject = (userid, userbase)
 with open(PIK,"wb") as f:
     pickle.dump(saveObject, f)
 
-print(userbase[0][0].userid)
+# print(userbase[0][0].username)
 userbase[0][0].add_friend(userbase[1][0])
-print(userbase[0][0].are_friends[userbase[1][0]])
+userbase[0][0].add_friend(userbase[2][0])
+userbase[0][0].add_friend(userbase[3][0])
+userbase[0][0].show_friends()

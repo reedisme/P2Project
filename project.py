e464a07e792ed48a5a728c321ca51a096dc77ff3
nnschrock <33878726+nnschrock@users.noreply.github.com>
2017-11-29 14:51:35 -0500

    Home function features
    Copy the home function into the rest of the program. Also make sure to add the line 
    self.posthist = []
    to the user class __init__

--- project.py
+++ project.py
@@ -1,80 +1,119 @@
 import pickle
-#Pickle allows us to save our users in a separate file for easy access.
-
-class Graph:
-    def __init__(self):
-        self.matrix = {}
-
-    def add_user(self, key):
-        self.matrix[key] = []
-
-    def add_friendship(self, v1, v2):
-        if v1 in self.matrix and v2 in self.matrix:
-            self.matrix[v1].append(v2)
-            self.matrix[v1].sort()
-        else:
-            print("ERROR: One or both vertices do not yet exist")
-        print(self.matrix)
-
-    def is_edge(self, v1, v2):
-        if v2 in self.matrix[v1]:
-            return True
-        else:
-            return False
-
-friendsdb = Graph()
-
+import time
 #Python mock social media website. We start by creating a user class
 class User:
     """This Class defines a user in our domain."""
-    userid = 0
 
-    def __init__(self, first, last):
-        self.first     = first
-        self.last      = last
+    def __init__(self, username, password, userid):
+        self.username  = username
+        self.password  = password
+        self.userid    = userid
+        self.posthist = []
+        # self.first     = first
+        # self.last      = last
         # self.birthday  = birthday
         self.friends   = []
-        User.userid += 1
-        friendsdb.add_user(self.userid)
 
     def add_friend(self, friend):
-        if self.userid > friend.userid:
-            friendsdb.add_friendship(self.userid, friend.userid)
-        elif friend.userid > self.userid:
-            friendsdb.add_friendship(friend.userid, self.userid)
+        self.friends.append(friend)
 
-    def are_friends(self, friend):
-        if self.userid > friend.userid:
-            return friendsdb.is_edge(self.userid, friend.userid)
-        elif friend.userid > self.userid:
-            return friendsdb.is_edge(friend.userid, self.userid)
+    def show_username(self):
+        return self.username
 
-    def generate_flist(self):
-        flist = []
-        for i in friendsdb.matrix[self]:
-            flist.append(i)
-        for i in friendsdb.matrix:
-            if self.userid in friendsdb.matrix[i]:
-                flist.append(i)
-        return(flist)
+    def show_friends(self):
+        for i in self.friends:
+            print(i.show_username())
 
-#Storing our userbase to the pickle file 'data.pk.'
-data = 'data.pk'
-userbase = pickle.load( open( data, 'rb' ) )
 
-#TODO 2 to login.
+#Storing our userbase and users to the pickle file 'data.pk.'
+PIK = 'data.pk'
 
-# start = int(input("Welcome to LeaFacebook. Press 1 to sign up."))
+with open(PIK, "rb") as f:
+    objects = pickle.load(f)
+
+userid, userbase = objects[0], objects[1]
+#userid, userbase = 0, []
+
+start = int(input("Welcome to LeaFacebook.\n Press 1 to sign up.\n Press 2 to login "))
+
+def getId(n):
+    for i in userbase:
+        if i[1] == n:
+            return i[3]
+    print("This is not a valid username")
+
+def lookup(id):
+    """Look up a persons username and password"""
+    for i in userbase:
+        if int(i[3]) == int(id):
+            password = i[2]
+            return password
+    print("This is not a valid username")
+
+def login():
+    username = input("Please input your username: ")
+    password = input("Please input your password: ")
+    id = getId(username)
+    passwd = lookup(id)
+    if passwd == password:
+        for i in userbase:
+            if username == i[1]:
+                return i[0]
+    else:
+        print("Please try again")
+        login()
 
 def userGenerator():
-    firstname = input("Please input your first name: ")
-    lastname = input("Please input your last name: ")
-    user = User(firstname, lastname)
-    userbase.append((user, user.userid, user.first))
+    global userid
+    username = input("Please input a username: ")
+    password = input("Please input a password: ")
+    userid += 1
+    user = User(username, password, userid)
+    userbase.append((user, username, password, userid))
+    return userbase
 
-# userGenerator()
-# print(userbase)
+def home(user):
+    print("Recent posts from your friends!")    # This section prints out the five most recent posts from your friends
+    tempposts = []
+    allfriends = []
+    for i in user.friends:
+        allfriends.append(i.posthist[-1])
+        allfriends.sort(reverse=True)
+    for i in range(5):
+        tempposts.append(allfriends[i])
+    for i in tempposts:
+        print(" ")
+        print("Posted on " + time.gmtime(tempposts[0]) + ":")
+        print(i[1])
 
-pickle.dump( userbase, open( data, 'wb' ) )
+    choice = input("Press 1 to make a new post, 2 to view a friend's profile, or 3 to log out.")
+    if choice == 1:
+        post = str(input("Enter the text of your new post here: "))
+        user.posthist.append([int(time.time()), post])  # Clunky implementation! Time first so sorting is easy
+        home(user)
+    elif choice == 2:
+        friend = eval(input("Whose profile would you like to see?"))  # Currently assuming this is the object ref.
+        print("You are viewing " + friend.username + "'s profile:")
+        print("User ID: " + friend.userid)
+        print("Friends:")
+        friend.show_friends()   # Currently we don't have any more profile info, but we can add it here if/when we do
+
+
+
+def main(s):
+    if s == 1:
+        userGenerator()
+    elif s == 2:
+        home(login())
+
+main(start)
+
+saveObject = (userid, userbase)
+with open(PIK,"wb") as f:
+    pickle.dump(saveObject, f)
+
+# print(userbase[0][0].username)
 userbase[0][0].add_friend(userbase[1][0])
-print(userbase[0][0].are_friends(userbase[1][0]))
+userbase[0][0].add_friend(userbase[2][0])
+userbase[0][0].add_friend(userbase[3][0])
+userbase[0][0].show_friends()

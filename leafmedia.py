de2c071996c87514d773aa3af7e4bd4db4bb90f7
reedisme <reedyoder1@gmail.com>
2017-12-05 15:17:05 -0500

    made the home function much better...

--- leafmedia.py
+++ leafmedia.py
@@ -13,11 +13,17 @@ class User:
         # self.first     = first
         # self.last      = last
         # self.birthday  = birthday
-        self.friends = []   # objects
-        self.posthist = []
+        self.friends = []
+        self.posts = []
 
     def add_friend(self, friend):
-        self.friends.append(friend)
+        if friend not in self.friends:
+            self.friends.append(friend)
+            friend.friends.append(self)
+
+    def remove_friend(self, friend):
+        self.friends.remove(friend)
+        friend.friends.remove(friend)
 
     def show_username(self):
         return self.username
@@ -26,6 +32,11 @@ class User:
         for i in self.friends:
             print(i.show_username())
 
+    def clear_friends(self):
+        self.friends = []
+
+    def add_post(self, post):
+        self.posts.append(post)
 
 # Storing our userbase and users to the pickle file 'data.pk.'
 PIK = 'data.pk'
@@ -34,16 +45,15 @@ with open(PIK, "rb") as f:
     objects = pickle.load(f)
 
 userid, userbase = objects[0], objects[1]
-# userid, userbase = 0, []
+#userid, userbase = 0, []
 
 start = int(input("Welcome to LeaFacebook.\n Press 1 to sign up.\n Press 2 to login "))
 
 
 def getObject(n):
-    """Get object from username"""
+    """Get id from username"""
     for i in userbase:
-        print(i[1], n)
-        if str(i[1]) == str(n):
+        if i[1] == str(n):
             return i[0]
     print("This is not a valid username")
 
@@ -64,8 +74,8 @@ def isuser(u):
 
 
 def login():
-    username = str(input("Please input your username: "))
-    password = str(input("Please input your password: "))
+    username = input("Please input your username: ")
+    password = input("Please input your password: ")
     uname, passwd = lookup(getObject(username))
     if passwd == password:
         for i in userbase:
@@ -80,49 +90,63 @@ def userGenerator():
     global userid
     username = input("Please input a username: ")
     if isuser(username) == False:
+        password = input("Please input a password: ")
         userid += 1
         user = User(username, password, userid)
+        user.add_post("I just joined LeafMedia, and I could not be happier!")
         userbase.append((user, username, password, userid))
     else:
         print("That username is taken. Please input a different username.")
         userGenerator()
-    password = input("Please input a password: ")
+
 
 
 def home(user):
-    print("Recent posts from your friends!")    # This section prints out the five most recent posts from your friends
-    tempposts = []
-    allfriends = []
-    for i in getObject(user).friends:
-        tempfriend = i
-        print(tempfriend)
-        allfriends.append(tempfriend.posthist[-1])
-        allfriends.sort()
-    for i in range(len(allfriends)):
-        tempposts.append(allfriends.pop())
-    for i in tempposts:
-        print(" ")
-        print("Posted on " + time.gmtime(tempposts[0]) + ":")
-        print(i[1])
-
-    choice = int(input("Press 1 to make a new post, 2 to view a friend's profile, 3 to add a friend, or 4 to log out."))
-    if choice == 1:
-        post = str(input("Enter the text of your new post here: "))
-        getObject(user).posthist.append([int(time.time()), post])  # Clunky implementation! Time first so sorting is easy
+    """Home function using user object."""
+    print(userbase)
+    print("Your friends:", user.friends)  # This section prints out the five most recent posts from your friends'
+    if len(user.friends) == 0:
+        print("You haven't added any friends yet! Here are some suggested users based on your area.")
+        counter = 1
+        for i in userbase:
+            print(counter, i[1])
+            counter += 1
+        friendToAdd = str(input("Input a username to add them as your friend: "))
+        while friendToAdd != str(0):
+            if isuser(friendToAdd) == True:
+                user.add_friend(getObject(friendToAdd))
+                print(friendToAdd, "is now your friend!")
+            else:
+                print("This user does not exist.")
+            friendToAdd = str(input("Input another friend, or input 0 to exit: "))
         home(user)
-    elif choice == 2:
-        friend = eval(input("Whose profile would you like to see?"))
-        print("You are viewing " + friend + "'s profile:")
-        print("Friends:")
-        getObject(friend).show_friends()   # Currently we don't have any more profile info, but we can add it here if/when we do
-    elif choice == 3:
-        friend = eval(input("Who would you like to add as a friend?"))
-        user.add_friend(getObject(friend))
-        # counter = 1
-        # for i in userbase:
-        #     print(counter, "   ", i[1])
-        #     counter += 1
-        # user.add_friend(input("Press the number corresponding to the new friend's username: "))
+    else:
+        for i in range(100):
+            print(" ")
+        print("Recent posts from your friends! \n")
+        for i in user.friends:
+            username, probablynotapassword = lookup(i)
+            print(username, "says:", i.posts[-1], "\n")
+        action = str(input("Press 1 to add a new friend, 2 to post, and 3 to view a friend's profile.\n"))
+        if action == str(1):
+            counter = 1
+            for i in userbase:
+                print(counter, i[1])
+                counter += 1
+            friendToAdd = str(input("Input a username to add them as your friend: "))
+            while friendToAdd != str(0):
+                if isuser(friendToAdd) == True:
+                    user.add_friend(getObject(friendToAdd))
+                    print(friendToAdd, "is now your friend!")
+                else:
+                    print("This user does not exist.")
+                friendToAdd = str(input("Input another friend, or input 0 to exit: "))
+            home(user)
+        if action == str(2):
+            mypost = input("What's on your mind?")
+            user.posts.append(mypost)
+
+    home(user)
 
 
 def main(s):
@@ -130,8 +154,7 @@ def main(s):
         userGenerator()
     elif s == 2:
         u = login()
-        usr, pswd = lookup(u)
-        home(usr)
+        home(u)
 
 
 print(userbase)

00417b356fe82c0cb563646cb423a65f74f9702c
nnschrock <33878726+nnschrock@users.noreply.github.com>
2017-12-08 14:57:31 -0500

    Add files via upload

--- project.py
+++ project.py
@@ -1,21 +1,29 @@
 import pickle
 import time
-#Python mock social media website. We start by creating a user class
+
+
+# Python mock social media website. We start by creating a user class
 class User:
     """This Class defines a user in our domain."""
 
     def __init__(self, username, password, userid):
-        self.username  = username
-        self.password  = password
-        self.userid    = userid
-        self.posthist = []
+        self.username = username
+        self.password = password
+        self.userid = userid
         # self.first     = first
         # self.last      = last
         # self.birthday  = birthday
-        self.friends   = []
+        self.friends = []
+        self.posts = []
 
     def add_friend(self, friend):
-        self.friends.append(friend)
+        if friend not in self.friends:
+            self.friends.append(friend)
+            friend.friends.append(self)
+
+    def remove_friend(self, friend):
+        self.friends.remove(friend)
+        friend.friends.remove(friend)
 
     def show_username(self):
         return self.username
@@ -24,8 +32,13 @@ class User:
         for i in self.friends:
             print(i.show_username())
 
+    def clear_friends(self):
+        self.friends = []
 
-#Storing our userbase and users to the pickle file 'data.pk.'
+    def add_post(self, post):
+        self.posts.append({post:[]})
+
+# Storing our userbase and users to the pickle file 'data.pk.'
 PIK = 'data.pk'
 
 with open(PIK, "rb") as f:
@@ -34,27 +47,35 @@ with open(PIK, "rb") as f:
 userid, userbase = objects[0], objects[1]
 #userid, userbase = 0, []
 
-start = int(input("Welcome to LeaFacebook.\n Press 1 to sign up.\n Press 2 to login "))
 
-def getId(n):
+
+def getObject(n):
+    """Get id from username"""
     for i in userbase:
-        if i[1] == n:
-            return i[3]
+        if i[1] == str(n):
+            return i[0]
     print("This is not a valid username")
 
-def lookup(id):
-    """Look up a persons username and password"""
+
+def lookup(obj):
+    """Look up a persons username and password given user object"""
     for i in userbase:
-        if int(i[3]) == int(id):
-            password = i[2]
-            return password
-    print("This is not a valid username")
+        if i[0] == obj:
+            return i[1], i[2]
+    print("This is not a valid user")
+
+
+def isuser(u):
+    for i in userbase:
+        if u == i[1]:
+            return True
+    return False
+
 
 def login():
     username = input("Please input your username: ")
     password = input("Please input your password: ")
-    id = getId(username)
-    passwd = lookup(id)
+    uname, passwd = lookup(getObject(username))
     if passwd == password:
         for i in userbase:
             if username == i[1]:
@@ -63,57 +84,122 @@ def login():
         print("Please try again")
         login()
 
+
 def userGenerator():
     global userid
     username = input("Please input a username: ")
-    password = input("Please input a password: ")
-    userid += 1
-    user = User(username, password, userid)
-    userbase.append((user, username, password, userid))
-    return userbase
+    if isuser(username) == False:
+        password = input("Please input a password: ")
+        userid += 1
+        user = User(username, password, userid)
+        user.add_post("I just joined LeafMedia, and I could not be happier!")
+        userbase.append((user, username, password, userid))
+    else:
+        print("That username is taken. Please input a different username.")
+        userGenerator()
+    return user
 
-def home(user):
-    print("Recent posts from your friends!")    # This section prints out the five most recent posts from your friends
-    tempposts = []
-    allfriends = []
-    for i in user.friends:
-        allfriends.append(i.posthist[-1])
-        allfriends.sort(reverse=True)
-    for i in range(5):
-        tempposts.append(allfriends[i])
-    for i in tempposts:
+def clear():
+    for i in range(100):
         print(" ")
-        print("Posted on " + time.gmtime(tempposts[0]) + ":")
-        print(i[1])
 
-    choice = input("Press 1 to make a new post, 2 to view a friend's profile, or 3 to log out.")
-    if choice == 1:
-        post = str(input("Enter the text of your new post here: "))
-        user.posthist.append([int(time.time()), post])  # Clunky implementation! Time first so sorting is easy
+def commentOn(user, index):
+    if type(index) != int:
+        print("Not int")
+    elif 0 < index and index <= len(user.posts):
+        comment = str(input("Please enter your comment here: "))
+        val = user.posts[index-1].keys
+        for i in val:
+            user.posts[index - 1][i].append(comment)
+        print(user.posts[index-1])
+def home(user):
+    """Home function using user object."""
+    print(userbase)
+    print("Your friends:", user.friends)  # This section prints out the five most recent posts from your friends'
+    if len(user.friends) == 0:
+        print("You haven't added any friends yet! Here are some suggested users based on your area.")
+        counter = 1
+        for i in userbase:
+            print(counter, i[1])
+            counter += 1
+        friendToAdd = str(input("Input a username to add them as your friend: "))
+        while friendToAdd != str(0):
+            if isuser(friendToAdd) == True:
+                user.add_friend(getObject(friendToAdd))
+                print(friendToAdd, "is now your friend!")
+            else:
+                print("This user does not exist.")
+            friendToAdd = str(input("Input another friend, or input 0 to exit: "))
         home(user)
-    elif choice == 2:
-        friend = eval(input("Whose profile would you like to see?"))  # Currently assuming this is the object ref.
-        print("You are viewing " + friend.username + "'s profile:")
-        print("User ID: " + friend.userid)
-        print("Friends:")
-        friend.show_friends()   # Currently we don't have any more profile info, but we can add it here if/when we do
+    else:
+        clear()
+        print("Recent posts from your friends! \n")
+        for i in user.friends:
+            username, probablynotapassword = lookup(i)
+            print(username, "says:", i.posts[-1], "\n")
+        action = str(input("Press 1 to add a new friend, 2 to post, and 3 to view a friend's profile, and 4 to logout.\n"))
+        if action == str(1):
+            counter = 1
+            for i in userbase:
+                print(counter, i[1])
+                counter += 1
+            friendToAdd = str(input("Input a username to add them as your friend: "))
+            while friendToAdd != str(0):
+                if isuser(friendToAdd) == True:
+                    user.add_friend(getObject(friendToAdd))
+                    print(friendToAdd, "is now your friend!")
+                else:
+                    print("This user does not exist.")
+                friendToAdd = str(input("Input another friend, or input 0 to exit: "))
+            home(user)
+        if action == str(2):
+            mypost = input("What's on your mind?")
+            user.posts.append(mypost)
+        if action == str(3):
+            print(user)
+            for i in user.friends:
+                friend, nothing = lookup(i)
+                print(friend)
+            friendToView = input("Please select a friends profile that you would like to view: ")
+            if isuser(friendToView) == True:
+                print(friendToView + "'s profile:")
+                for i in getObject(friendToView).posts:
+                    print(i)
+                index = int(input("Select which post you would like to comment on, or press 0 to exit!"))
+                if index != 0:
+                    commentOn(getObject(friendToView), index)
+                else:
+                    home(user)
+            else:
+                print("Bad user")
 
+        if action == str(4):
+            clear()
+            start = int(input("Welcome to LeaFacebook.\n Press 1 to sign up.\n Press 2 to login"))
+            main(start)
+    home(user)
 
+clear()
+start = int(input("Welcome to LeaFacebook.\n Press 1 to sign up.\n Press 2 to login"))
 
 def main(s):
     if s == 1:
-        userGenerator()
+        u = userGenerator()
+        home(u)
     elif s == 2:
-        home(login())
+        u = login()
+        home(u)
 
+
+print(userbase)
 main(start)
 
 saveObject = (userid, userbase)
-with open(PIK,"wb") as f:
+with open(PIK, "wb") as f:
     pickle.dump(saveObject, f)
 
-# print(userbase[0][0].username)
-userbase[0][0].add_friend(userbase[1][0])
-userbase[0][0].add_friend(userbase[2][0])
-userbase[0][0].add_friend(userbase[3][0])
-userbase[0][0].show_friends()
+    # print(userbase[0][0].username)
+    # userbase[0][0].add_friend(userbase[1][0])
+    # userbase[0][0].add_friend(userbase[2][0])
+    # userbase[0][0].add_friend(userbase[3][0])
+    # userbase[0][0].show_friends()

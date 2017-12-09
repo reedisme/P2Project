7ee7f30af284b525e7a01493ceced0ce57567625
nnschrock <33878726+nnschrock@users.noreply.github.com>
2017-12-09 12:22:48 -0500

    Probably good enough, no major problems

--- leafmedia.py
+++ leafmedia.py
@@ -47,7 +47,7 @@ userbase = pickle.load( open( PIK, "rb" ))
 
 def quitprogram():
     pickle.dump(userbase, open(PIK, "wb"))
-    sys.exit() 
+    sys.exit()
 
 def getObject(n):
     """Get id from username"""
@@ -73,8 +73,8 @@ def isuser(u):
 
 
 def login():
-    username = input("Please input your username: ")
-    password = input("Please input your password: ")
+    username = input("Please input your username: \n")
+    password = input("Please input your password: \n")
     uname, passwd = lookup(getObject(username))
     if passwd == password:
         for i in userbase:
@@ -86,9 +86,9 @@ def login():
 
 
 def userGenerator():
-    username = input("Please input a username: ")
+    username = input("Please input a username: \n")
     if isuser(username) == False:
-        password = input("Please input a password: ")
+        password = input("Please input a password: \n")
         u = User(username, password)
         u.add_post("I just joined LeafMedia, and I could not be happier!")
         userbase.append((u, username, password))
@@ -103,10 +103,14 @@ def clear():
 
 def commentOn(user, index):
     if type(index) is not int:
-        print("Not int")
+        print("Not integer")
     elif 0 < index and index <= len(user.posts):
         val = user.posts[index-1]
-        print(val)
+        print(val[0])
+        print("\nComments:\n")
+        if len(val[1]) > 0:
+            for i in val[1][1:-1]:
+                print(i, "\n\n")
         comment = str(input("Add your comment here: "))
         val[1].append(comment)
         print(val)
@@ -125,14 +129,14 @@ def home(user):
         for i in userbase:
             print(counter, i[1])
             counter += 1
-        friendToAdd = str(input("Input a username to add them as your friend: "))
+        friendToAdd = str(input("Input a username to add them as your friend: \n"))
         while friendToAdd != str(0):
             if isuser(friendToAdd) == True:
                 user.add_friend(getObject(friendToAdd))
                 print(friendToAdd, "is now your friend!")
             else:
                 print("This user does not exist.")
-            friendToAdd = str(input("Input another friend, or input 0 to exit: "))
+            friendToAdd = str(input("Input another friend, or input 0 to exit: \n"))
         home(user)
     else:
         clear()
@@ -140,41 +144,45 @@ def home(user):
         for i in user.friends:
             username, probablynotapassword = lookup(i)
             print(username, "says:", i.posts[-1][0], "\n")
-            if len(i.posts[-1][1]) > 0: 
+            if len(i.posts[-1][1]) > 0:
                 print("    Comments:")
                 for i in i.posts[-1][1]:
-                    print(i)
-        action = str(input("Press 1 to add a new friend, 2 to post, and 3 to view a friend's profile, and 4 to logout.\n"))
+                    print("    " + i + "\n")
+        action = str(input("Press 1 to add a new friend.\n"
+                           "Press 2 to post.\n"
+                           "Press 3 to view a friend's profile.\n"
+                           "Press 4 to logout.\n"))
         if action == str(1):
             counter = 1
             for i in userbase:
                 print(counter, i[1])
                 counter += 1
-            friendToAdd = str(input("Input a username to add them as your friend: "))
+            friendToAdd = str(input("Input a username to add them as your friend: \n"))
             while friendToAdd != str(0):
                 if isuser(friendToAdd) == True:
                     user.add_friend(getObject(friendToAdd))
                     print(friendToAdd, "is now your friend!")
                 else:
                     print("This user does not exist.")
-                friendToAdd = str(input("Input another friend, or input 0 to exit: "))
+                friendToAdd = str(input("Input another friend, or input 0 to exit: \n"))
             home(user)
         if action == str(2):
-            mypost = input("What's on your mind?")
-            user.posts.append(mypost)
+            mypost = str(input("What's on your mind? "))
+            user.add_post(mypost)
         if action == str(3):
             print(user)
             for i in user.friends:
                 friend, nothing = lookup(i)
                 print(friend)
-            friendToView = input("Please select a friends profile that you would like to view: ")
+            friendToView = input("Please select a friends profile that you would like to view: \n")
             if isuser(friendToView) == True:
                 print(friendToView + "'s profile:")
                 counter = 0
                 for i in getObject(friendToView).posts:
                     counter += 1
                     print(counter, i[0])
-                post = int(input("Select which post you would like to comment on, or press 0 to exit!"))
+                post = int(input("Select which post you would like to comment on by pressing the corresponding number"
+                                 ", or press 0 to exit.\n"))
                 if post != 0:
                     commentOn(getObject(friendToView), post)
             else:
@@ -182,12 +190,12 @@ def home(user):
 
         if action == str(4):
             clear()
-            start = int(input("Welcome to LeaFacebook.\n Press 1 to sign up.\n Press 2 to login.\n Press 3 to quit: "))
+            start = int(input("Welcome to LeafMedia!\n Press 1 to sign up.\n Press 2 to login.\n Press 3 to quit.\n"))
             main(start)
     home(user)
 
 clear()
-start = int(input("Welcome to LeaFacebook.\n Press 1 to sign up.\n Press 2 to login.\n Press 3 to quit: "))
+start = int(input("Welcome to LeafMedia!\n Press 1 to sign up.\n Press 2 to login.\n Press 3 to quit: \n"))
 
 def main(s):
     if s == 1:
